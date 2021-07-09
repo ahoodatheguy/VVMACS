02e67c417343d3b45b07068c7366304addb5c87c
Ahadu Kebede <ahadukebede@gmail.com>
2021-07-09 14:30:54 -0400

    Added python support

---
+++ elpa/lsp-mode-20210707.1922/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20210707.1922" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "f96c7ad1b2ddb456fafde00cbf07ed3d27d0ce0b" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
