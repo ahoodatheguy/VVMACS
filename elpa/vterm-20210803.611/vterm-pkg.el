b6cb7ac2d36d56fd1c26846b82126ee0061e264b
Ahadu Kebede <Ahadu Kebede>
2021-08-03 21:47:37 -0400

    Added term mode configuration

---
+++ elpa/vterm-20210803.611/vterm-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vterm" "20210803.611" "Fully-featured terminal emulator"
+  '((emacs "25.1"))
+  :commit "0ec407841ddc81fa4546abe115c12eb7c6c71521" :authors
+  '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
+  :maintainer
+  '("Lukas Fürmetz" . "fuermetz@mailbox.org")
+  :keywords
+  '("terminals")
+  :url "https://github.com/akermu/emacs-libvterm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
